856aa7223d3406f97ee5f5ac54714e876c46e23f
Thang0226 <thang.nd0226@gmail.com>
2024-10-21 16:46:17 +0700

    add base test cases

--- test/FizzBuzzTranslateTest.java
+++ test/FizzBuzzTranslateTest.java
@@ -18,5 +18,42 @@ public class FizzBuzzTranslateTest {
 		assertEquals(expected, result);
 	}
 
-	
+	@Test
+	@DisplayName("Return 'Buzz' when divisible by 5")
+	public void testReturnFizzWhenDivisibleBy5() {
+		// arrange
+		int number = 25;
+		String expected = "Buzz";
+		// action
+		FizzBuzzTranslate translator = new FizzBuzzTranslate();
+		String result = translator.translate(number);
+		// assert
+		assertEquals(expected, result);
+	}
+
+	@Test
+	@DisplayName("Return 'FizzBuzz' when divisible by 3 and 5")
+	public void testReturnFizzWhenDivisibleBy3And5() {
+		// arrange
+		int number = 30;
+		String expected = "FizzBuzz";
+		// action
+		FizzBuzzTranslate translator = new FizzBuzzTranslate();
+		String result = translator.translate(number);
+		// assert
+		assertEquals(expected, result);
+	}
+
+	@Test
+	@DisplayName("Return 'number' when not divisible by 3 or 5")
+	public void testReturnStringWhenNotDivisibleBy3Or5() {
+		// arrange
+		int number = 29;
+		String expected = "29";
+		// action
+		FizzBuzzTranslate translator = new FizzBuzzTranslate();
+		String result = translator.translate(number);
+		// assert
+		assertEquals(expected, result);
+	}
 }
