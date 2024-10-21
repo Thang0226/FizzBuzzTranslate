75e9553e7ffdb185cd0bd40b1193ff58d64bd9b1
Thang0226 <thang.nd0226@gmail.com>
2024-10-21 21:23:18 +0700

    return pronunciation of number with non-FizzBuzz numbers

--- src/FizzBuzzTranslate.java
+++ src/FizzBuzzTranslate.java
@@ -1,5 +1,6 @@
 public class FizzBuzzTranslate {
-	public String translate(int number) {
+	public String FizzBuzztranslate(int number) {
+		final String CONCAT_STR = " ";
 		boolean divisibleBy3 = number % 3 == 0;
 		boolean divisibleBy5 = number % 5 == 0;
 
@@ -15,6 +16,32 @@ public class FizzBuzzTranslate {
 			return "Buzz";
 		}
 
-		return number_str;
+		if (number < 10) {
+			return translate(number_str.charAt(0));
+		} else if (number < 20) {
+			return "muoi" + CONCAT_STR + translate(number_str.charAt(1));
+		} else if (number < 100) {
+			String firstWord = translate(number_str.charAt(0));
+			String secondWord = translate(number_str.charAt(1));
+			return firstWord + CONCAT_STR + secondWord;
+		} else {
+			return "";
+		}
+	}
+
+	private String translate(char numChar) {
+		return switch (numChar) {
+			case '0' -> "muoi";
+			case '1' -> "mot";
+			case '2' -> "hai";
+			case '3' -> "ba";
+			case '4' -> "bon";
+			case '5' -> "nam";
+			case '6' -> "sau";
+			case '7' -> "bay";
+			case '8' -> "tam";
+			case '9' -> "chin";
+			default -> "";
+		};
 	}
 }
--- test/FizzBuzzTranslateTest.java
+++ test/FizzBuzzTranslateTest.java
@@ -13,7 +13,7 @@ public class FizzBuzzTranslateTest {
 		String expected = "Fizz";
 		// action
 		FizzBuzzTranslate translator = new FizzBuzzTranslate();
-		String result = translator.translate(number);
+		String result = translator.FizzBuzztranslate(number);
 		// assert
 		assertEquals(expected, result);
 	}
@@ -26,7 +26,7 @@ public class FizzBuzzTranslateTest {
 		String expected = "Buzz";
 		// action
 		FizzBuzzTranslate translator = new FizzBuzzTranslate();
-		String result = translator.translate(number);
+		String result = translator.FizzBuzztranslate(number);
 		// assert
 		assertEquals(expected, result);
 	}
@@ -39,23 +39,23 @@ public class FizzBuzzTranslateTest {
 		String expected = "FizzBuzz";
 		// action
 		FizzBuzzTranslate translator = new FizzBuzzTranslate();
-		String result = translator.translate(number);
+		String result = translator.FizzBuzztranslate(number);
 		// assert
 		assertEquals(expected, result);
 	}
 
-	@Test
-	@DisplayName("Return 'number' when not divisible by 3 or 5")
-	public void testReturnStringWhenNotDivisibleBy3Or5() {
-		// arrange
-		int number = 29;
-		String expected = "29";
-		// action
-		FizzBuzzTranslate translator = new FizzBuzzTranslate();
-		String result = translator.translate(number);
-		// assert
-		assertEquals(expected, result);
-	}
+//	@Test
+//	@DisplayName("Return 'number' when not divisible by 3 or 5")
+//	public void testReturnStringWhenNotDivisibleBy3Or5() {
+//		// arrange
+//		int number = 29;
+//		String expected = "29";
+//		// action
+//		FizzBuzzTranslate translator = new FizzBuzzTranslate();
+//		String result = translator.FizzBuzztranslate(number);
+//		// assert
+//		assertEquals(expected, result);
+//	}
 
 	@Test
 	@DisplayName("Return 'Fizz' when number includes digit 3")
@@ -65,7 +65,7 @@ public class FizzBuzzTranslateTest {
 		String expected = "Fizz";
 		// action
 		FizzBuzzTranslate translator = new FizzBuzzTranslate();
-		String result = translator.translate(number);
+		String result = translator.FizzBuzztranslate(number);
 		// assert
 		assertEquals(expected, result);
 	}
@@ -78,7 +78,7 @@ public class FizzBuzzTranslateTest {
 		String expected = "Buzz";
 		// action
 		FizzBuzzTranslate translator = new FizzBuzzTranslate();
-		String result = translator.translate(number);
+		String result = translator.FizzBuzztranslate(number);
 		// assert
 		assertEquals(expected, result);
 	}
@@ -91,7 +91,33 @@ public class FizzBuzzTranslateTest {
 		String expected = "FizzBuzz";
 		// action
 		FizzBuzzTranslate translator = new FizzBuzzTranslate();
-		String result = translator.translate(number);
+		String result = translator.FizzBuzztranslate(number);
+		// assert
+		assertEquals(expected, result);
+	}
+
+	@Test
+	@DisplayName("Return number pronunciation with non-Fizz/Buzz case 1")
+	public void testReturnNumberPronunciationWithNonFizzBuzzCase1() {
+		// arrange
+		int number = 11;
+		String expected = "muoi mot";
+		// action
+		FizzBuzzTranslate translator = new FizzBuzzTranslate();
+		String result = translator.FizzBuzztranslate(number);
+		// assert
+		assertEquals(expected, result);
+	}
+
+	@Test
+	@DisplayName("Return number pronunciation with non-Fizz/Buzz case 2")
+	public void testReturnNumberPronunciationWithNonFizzBuzzCase2() {
+		// arrange
+		int number = 89;
+		String expected = "tam chin";
+		// action
+		FizzBuzzTranslate translator = new FizzBuzzTranslate();
+		String result = translator.FizzBuzztranslate(number);
 		// assert
 		assertEquals(expected, result);
 	}
